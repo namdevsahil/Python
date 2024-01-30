fff247ff30d0284582528f88aa803e1a6bc3eb46
sahil namdev <118657806+namdevsahil@users.noreply.github.com>
2024-01-31 01:47:30 +0530

    Add files via upload

---
+++ Python-list-function.py
@@ -0,0 +1,13 @@
+list = ["sahil", "abc", "numeric", "zoya"]
+print(sorted(list))
+
+list1 = [1,9,3,6,2]
+print(sorted(list1))
+
+
+list = ["sahil", "abc", "numeric", "zoya","sahil","zoya"]
+print(list.count("zoya"))
+
+list = [1,2,3,4,5,6]
+list.reverse()
+print(list)
